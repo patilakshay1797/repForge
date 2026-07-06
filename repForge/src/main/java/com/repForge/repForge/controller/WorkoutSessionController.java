eebcf3008ffe20eee5cf1b9fffaf20f5699a25f5
patilakshay1797 <patilakshay1797@gmail.com>
2026-07-06 17:40:41 +0530

    basic apis created to add new workout session and to get all sessions

--- repForge/src/main/java/com/repForge/repForge/controller/WorkoutSessionController.java
+++ repForge/src/main/java/com/repForge/repForge/controller/WorkoutSessionController.java
@@ -3,16 +3,28 @@ package com.repForge.repForge.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.repForge.repForge.entity.WorkoutSession;
+import com.repForge.repForge.service.WorkoutSessionService;
 
 @RestController
 public class WorkoutSessionController {
 	
+	@Autowired
+	private WorkoutSessionService workService;
+	
 	@GetMapping("/getAllWorkoutSessions")
 	public List<WorkoutSession> getAllWorkoutSessions() {
-		return new ArrayList<>();
+		return workService.getAllWorkouts();
+	}
+	
+	@PostMapping("/workOutSession")
+	public String createWorkoutSession(@RequestBody WorkoutSession workSess) {
+		return workService.createWorkoutSession(workSess);
 	}
 }
--- repForge/src/main/java/com/repForge/repForge/service/WorkoutSessionService.java
+++ repForge/src/main/java/com/repForge/repForge/service/WorkoutSessionService.java
@@ -1,5 +1,7 @@
 package com.repForge.repForge.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,5 +19,10 @@ public class WorkoutSessionService {
 		if(obj != null)
 			return "Workout Session saved";
 		else return "Failed to save Workout Session";
+	}
+
+	public List<WorkoutSession> getAllWorkouts() {
+		
+		return workoutSessionRepo.findAll();
 	}
 }
